3b0f042729a69b7a9f99f08c96ac9d59d4d57208
ceamattos <carlos@sensio.com.br>
2022-02-12 19:35:24 -0300

    template addded

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,11 +1,30 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { document } from "../utils/dynamodbCliente";
+import * as handlebars from "handlebars";
+import { join } from "path";
+import { readFileSync } from "fs";
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
 }
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compile = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), "src", "templates", "certificate.hbs");
+
+  const html = readFileSync(filePath, "utf8");
+
+  return handlebars.compile(html)(data);
+};
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
@@ -26,6 +45,10 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       },
     })
     .promise();
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+  const medal = readFileSync(medalPath, "base64");
+  const content = await compile({ date: new Date().toISOString(), grade, id, medal, name });
 
   return {
     statusCode: 200,
